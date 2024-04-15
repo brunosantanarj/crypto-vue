e7f398264a3abcb85c9c8f2bf3bfb0fa29f23dff
Bruno <bruno@MacBodok-Pro-de-Bruno.local>
2024-04-15 08:28:26 -0300

    chore(vue-query): add vue-query plugin to vue instance

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueQueryPlugin)
 
 app.mount('#app')
